b8e1779df163a63266fb232490372259fc6c2aa5
Martin Edgar Atole <107755242+CS-Martin@users.noreply.github.com>
2023-08-20 15:03:25 +0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,211 +1,140 @@
-; Defining vars
-sys_exit equ 1 ; System Exit
-sys_read equ 3
-sys_write equ 4
-stdin equ 0
-stdout equ 1
+; @Members
+; Martin Edgar Atole
+; Marco D. Mosna
+; Ben Jenon Alpuerto
 
-section .text
-    global _start
-
-    _start:
-        ; Print "enter first number: "
-        mov eax, sys_write
-        mov ebx, stdout
-        mov ecx, firstNum
-        mov edx, lenFirstNum
-        int 0x80
-        
-        ; Get input for first number
-        mov eax, sys_read
-        mov ebx, stdin
-        mov ecx, num1
-        mov edx, 2
-        int 0x80
-        
-        ; Print "enter second number: "
-        mov eax, sys_write
-        mov ebx, stdout
-        mov ecx, secondNum
-        mov edx, lenSecondNum
-        int 0x80
-        
-        ; Get input for the second number
-        mov eax, sys_read
-        mov ebx, stdin
-        mov ecx, num2
-        mov edx, 2
-        int 0x80
-        
-        ; Print "enter third number: "
-        mov eax, sys_write
-        mov ebx, stdout
-        mov ecx, thirdNum
-        mov edx, lenThirdNum
-        int 0x80
-        
-        ; Get input for the third number
-        mov eax, sys_read
-        mov ebx, stdin
-        mov ecx, num3
-        mov edx, 2
-        int 0x80
-        
-        ; Print "Largest Number: "
-        mov eax, sys_write
-        mov ebx, stdout
-        mov ecx, largest
-        mov edx, lenLargest
-        int 0x80
-        
-        ; Assembly interprets inputs as characters
-        ; So I need to convert it to integer
-        ; To do that:
-        mov al, [num1]      ; al is 8-bit char size register
-        sub al, '0'         ; convert to integer
-         
-        mov ah, [num2]
-        sub ah, '0'
-        
-        ; Compare num1 and num2 to determine which is greater or lesser
-        cmp al, ah ; al = num1, ah = num2
-        jg _greater ; Jump if greater
-        jl _lesser  ; Jump if lesser 
-        
-        ; If _greater, then num1 > num2
-        _greater:
-            ; Have to convert everything to integer once again
-            mov al, [num1]
-            sub al, '0'
-            mov ah, [num3]
-            sub ah, '0'
-            ; Compare num1 to num3
-            cmp al, ah
-            jl _printNum3
-            
-            ; Else num1 is greater
-            ; Print out num1
-            mov eax, sys_write
-            mov ebx, stdout
-            mov ecx, num1
-            mov edx, 2
-            int 0x80
-            jmp _leastVal
-        
-        ; if _lesser, num2 > num1
-        _lesser:
-            ; Convert to int
-            mov al, [num2]
-            sub al, '0'
-            mov ah, [num3]
-            sub ah, '0'
-            ; Compare num2 to num3
-            cmp al, ah
-            jl _printNum3
-            
-            ; Else num2 is greater
-            ; Print out num 2
-            mov eax, sys_write
-            mov ebx, stdout
-            mov ecx, num2
-            mov edx, 2
-            int 0x80
-            jmp _leastVal
-        
-        ; If neither num1 or num2, then num3 is greater
-        _printNum3:
-            mov eax, sys_write
-            mov ebx, stdout
-            mov ecx, num3
-            mov edx, 2
-            int 0x80
-            jmp _leastVal
-            
-        ; This is where finding the lowest value starts
-        _leastVal:
-            ; Print "Smallest Number: "
-            mov eax, sys_write
-            mov ebx, stdout
-            mov ecx, smallest
-            mov edx, lenSmallest
-            int 0x80
-            
-            ; Convert chars to integers once again
-            mov al, [num1]
-            sub al, '0'
-            mov ah, [num2] 
-            sub ah, '0'
-            cmp al, ah
-            jl _num1Lesser
-            jg _num2Lesser
-            
-            ; Then num1 < num2
-            _num1Lesser:
-                mov al, [num1]
-                sub al, '0'
-                mov ah, [num3]
-                sub ah, '0'
-                ; Compare which is the lowest
-                cmp al, ah
-                jg _printNum3Lowest
-                
-                ; Else, num1 is the lowest value among 3 numbers
-                ; Print num1
-                mov eax, sys_write
-                mov ebx, stdout
-                mov ecx, num1
-                mov edx, 2
-                int 0x80
-                
-            ; Then num2 < num1
-            _num2Lesser:
-                mov al, [num2]
-                sub al, '0'
-                mov ah, [num3]
-                sub ah, '0'
-                ; Compare which number is the lowest
-                cmp al, ah
-                jg _printNum3Lowest
-                
-                ; Else, num2 is the lowest value among the three
-                ; PRint num2
-                mov eax, sys_write
-                mov ebx, stdout
-                mov ecx, num2
-                mov edx, 2
-                int 0x80
-                
-            _printNum3Lowest:
-                mov eax, sys_write
-                mov ebx, stdout
-                mov ecx, num3
-                mov edx, 2
-                int 0x80
-            
-        ; System call to Exit
-        mov eax, 1
-        mov ebx, 0
-        int 0x80
-        
-; Section that contains uninitialized variables
-section .bss
-    ; 2 can only contain 1 digit variable
-    num1 resb 2 ; First var
-    num2 resb 2 ; Second var
-    num3 resb 2 ; Third var
+; Define - I like to do this because it feels like I'm using C++
+sys_exit    equ 1 ; System exit
+sys_write   equ 4 ; Writing
+sys_read    equ 3 ; Read inputs
+stdin       equ 0 ; input
+stdout      equ 1 ; output
 
 section .data
-    firstNum db 'Enter first number: '
-    lenFirstNum equ $ - firstNum
+    ; Target
+    ; target db 42
     
-    secondNum db 0xa, 'Enter second number: '
-    lenSecondNum equ $ - secondNum
+    prompt db 'Guess a number between 1 to 100: '
+    promptLen equ $ - prompt
     
-    thirdNum db 0xa, 'Enter third number: '
-    lenThirdNum equ $ - thirdNum
+    ; Message if user guessed the number
+    winner db 'Congratulations, you guessed the number correctly!'
+    winnerLen equ $ - winner
     
-    largest db 0xa, 'Largest Number: '
-    lenLargest equ $ - largest
+    ; Message if user guessed too high
+    too_high db 'Your guess is too high! Try again: '
+    highLen equ $ - too_high
     
-    smallest db 'Smallest Number: '
-    lenSmallest equ $ - smallest
+    ; Message if user guessed too low
+    too_low db 'Your guess is too low! Try again: '
+    lowLen equ $ - too_low
+    
+    ; Input variables
+    player_guess db 10
+    guessLen equ 10
+
+section .bss
+    target resb 1 
+    
+section .text
+    global _start
+    
+_start:
+    ; Generate a random number between 0 and 255 (one byte)
+    rdrand ax
+
+    ; Calculate the remainder when dividing the random value by 101 (0-100 inclusive)
+    xor edx, edx
+    mov ecx, 101
+    div cx
+
+    ; Store the random number in random_var
+    mov byte [target], dl
+
+    
+    ; Display prompt message to console
+    mov eax, sys_write
+    mov ebx, stdout
+    mov ecx, prompt
+    mov edx, promptLen
+    int 80h
+    
+; If user failed to guess the correct number
+; Call guess_number_loop
+guess_number_loop:
+    ; We read user's input here
+    mov eax, sys_read
+    mov ebx, stdin
+    mov ecx, player_guess
+    mov edx, guessLen
+    int 80h
+    
+    ; After getting the user's input
+    ; Convert the input into a digit
+    mov eax, 0
+    mov ecx, player_guess
+    
+convert_loop:
+    cmp byte [ecx], 10        ; Check if end of input
+    je check_users_guess
+    sub byte [ecx], '0'       ; Convert ASCII digit to number
+    imul eax, 10              ; Multiply the current number by 10
+    add al, [ecx]             ; Add the converted digit to the number
+    inc ecx                   ; Move to the next character
+    jmp convert_loop
+
+; This function will compare the number to the target number
+check_users_guess:
+    ; Compare user's input from the target number
+    cmp al, byte [target]
+    ; If less, jump to low function (jl = jump if less)
+    ; If equal, print congratualtions and exit program (je = jump if equal)
+    ; If greater, jump to high function (jg = jump if greater)
+    
+    je _guess_is_equal
+    jg _guess_is_high
+    jl _guess_is_less
+    
+_guess_is_less:
+    ; Display guess is too low message
+    mov eax, sys_write
+    mov ebx, stdout
+    mov ecx, too_low
+    mov edx, lowLen
+    int 80h
+    
+    ; Jump to guess_number_loop to prompt user
+    ; To guess the number again
+    jmp guess_number_loop
+    
+_guess_is_high:
+    ; Display guess is too high message    
+    mov eax, sys_write
+    mov ebx, stdout
+    mov ecx, too_high
+    mov edx, highLen
+    int 80h
+    
+    ; Jump to guess_number_loop to prompt user
+    ; To guess the number again
+    jmp guess_number_loop
+    
+_guess_is_equal:
+    ; Display congratulation message
+    mov eax, sys_write
+    mov ebx, stdout
+    mov ecx, winner
+    mov edx, winnerLen
+    int 80h
+    
+    ; Terminate
+    jmp end
+    
+end:
+    ; Terminate the program
+    mov eax, 1
+    int 80h
+    
+; @References
+; https://www.youtube.com/watch?v=Xw98IGQvUSo
+; and ofcourse, chatGPT
